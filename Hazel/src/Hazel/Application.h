4677fe883d68b5e7a72ed53519b58c394291da13
SeongBeom Choi <s004402@gmail.com>
2022-09-12 22:00:39 +0900

    Added layers and layer stack.

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include "Core.h"
-#include "Events/Event.h"
 #include "Window.h"
+#include "Hazel/LayerStack.h"
+#include "Hazel/Events/Event.h"
 #include "Hazel/Events/ApplicationEvent.h"
 
 namespace Hazel {
@@ -16,11 +17,16 @@ namespace Hazel {
 		void Run();
 
 		void OnEvnet(Event& e);
+
+		void PushLayer(Layer* layer);
+		void PushOverLay(Layer* layer);
+
 	private:
 		bool OnWindowClose(WindowCloseEvent& e);
 
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
+		LayerStack m_LayerStack;
 	};
 
 	// To be defined in CLIENT
